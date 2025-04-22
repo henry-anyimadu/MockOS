77f86a7444a21d6ac3e24a866b8b02d8406dc562
Henry Anyimadu <henry@whatsfabric.com>
2025-04-21 22:38:02 -0500

    Studio 17 num 4c (incomplete)

---
+++ include/mockos/SimpleFileSystem.h
@@ -0,0 +1,25 @@
+/**
+* Studios 16-21: SimpleFileSystem.h
+ **/
+#pragma once
+
+#include <filesystem>
+#include <map>
+#include <string>
+#include <set>
+#include "AbstractFileSystem.h"
+
+class SimpleFileSystem: AbstractFileSystem {
+ public:
+ SimpleFileSystem();
+ ~SimpleFileSystem();
+
+ std::map<std::string, AbstractFile*> files;
+ std::set<std::string> openFiles;
+
+ int addFile(std::string filename, AbstractFile *) override;
+ int createFile(std::string filename) override;
+ AbstractFile *openFile(std::string filename) override;
+ int closeFile(AbstractFile *) override;
+ int deleteFile(std::string filename) override;
+};
---
+++ lib/mockos/SimpleFileSystem.cpp
@@ -0,0 +1,15 @@
+/**
+ * Studios 16-21: SimpleFileSystem.cpp
+ **/
+#include "./mockos/SimpleFileSystem.h"
+#include <string>
+
+using namespace std;
+
+SimpleFileSystem::SimpleFileSystem() {
+    string filename;
+    AbstractFile* fs = new AbstractFile;
+    SimpleFileSystem::addFile(filename, fs) {
+
+    }
+}
