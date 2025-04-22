33291afd59eea98f53ae8db27e91a6aaeaf1f657
Henry Anyimadu <henry@whatsfabric.com>
2025-04-21 20:53:34 -0500

    num 2

--- include/mockos/TextFile.h
+++ include/mockos/TextFile.h
@@ -8,6 +8,10 @@ class TextFile: AbstractFile {
 public:
     TextFile(std::string input);
     unsigned int getSize();
+    std::string getName();
+    std::vector<char> append();
+    void read() override;
+    void write();
 private:
     std::string name;
     std::vector<char> contents;
